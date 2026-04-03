1e6ae35e44d32bf719f583fb2863906ced51b2d6
PatriciaChung64 <patricia199864@gmail.com>
2026-04-03 23:06:15 +0100

    Added checkbox to change completion status

--- moz-todo-app/src/To-do-list.jsx
+++ moz-todo-app/src/To-do-list.jsx
@@ -6,15 +6,15 @@ let uniqueId = 3;
 export default function TodoList() {
   const [liststate, setState] = useState([
     { "id": "0", "name": "Test 1", "completed": false}, 
-    { "id": "1", "name": "Test 2", "completed": false}, 
+    { "id": "1", "name": "Test 2", "completed": true}, 
     { "id": "2", "name": "Test 3", "completed": false}]);
 
   function addItem(formData) {
     const item = formData.get("item");
-    console.log("Added %s", item);
+    // console.log("Added %s", item);
     const newlist = [...liststate];
     // console.log("New element has id of %s", uniqueId);
-    newlist.push({"id": uniqueId + "", "name": item});
+    newlist.push({"id": uniqueId + "", "name": item, "completed": false});
     uniqueId++;
     setState(newlist);
   }
@@ -27,10 +27,19 @@ export default function TodoList() {
     setState(newlist);
   }
 
+  function changeCompletion(id) {
+    const newlist = [...liststate];
+    const index = newlist.findIndex((el) => el.id === id);
+    newlist[index].completed = !newlist[index].completed;
+    // console.log(newlist[index].completed);
+    setState(newlist);
+  }
+
   const listitems = liststate.map((item) => (
     <>
       <li key={item.id}>{item.name}</li>
       <button itemindex={item.id} onClick={(e) => deleteItem(e.target.attributes.itemindex.value)}>Delete</button>
+      <input type="checkbox" itemindex={item.id} checked={item.completed} onChange={(e) => changeCompletion(e.target.attributes.itemindex.value)} />
     </>
   ));
   return (
