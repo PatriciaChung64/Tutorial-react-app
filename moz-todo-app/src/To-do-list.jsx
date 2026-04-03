ddb644c4a19a7ec7415ef16a484dc6cf92735e2f
PatriciaChung64 <patricia199864@gmail.com>
2026-04-03 23:55:53 +0100

    Implemented an edit feature

--- moz-todo-app/src/To-do-list.jsx
+++ moz-todo-app/src/To-do-list.jsx
@@ -5,16 +5,16 @@ let uniqueId = 3;
 
 export default function TodoList() {
   const [liststate, setState] = useState([
-    { "id": "0", "name": "Test 1", "completed": false}, 
-    { "id": "1", "name": "Test 2", "completed": true}, 
-    { "id": "2", "name": "Test 3", "completed": false}]);
+    { "id": "0", "name": "Test 1", "completed": false, "editing": false}, 
+    { "id": "1", "name": "Test 2", "completed": true, "editing": false}, 
+    { "id": "2", "name": "Test 3", "completed": false, "editing": false}]);
 
   function addItem(formData) {
     const item = formData.get("item");
     // console.log("Added %s", item);
     const newlist = [...liststate];
     // console.log("New element has id of %s", uniqueId);
-    newlist.push({"id": uniqueId + "", "name": item, "completed": false});
+    newlist.push({"id": uniqueId + "", "name": item, "completed": false, "editing": false});
     uniqueId++;
     setState(newlist);
   }
@@ -35,9 +35,39 @@ export default function TodoList() {
     setState(newlist);
   }
 
+  function enterEditMode(id) {
+    const newlist = [...liststate];
+    const index = newlist.findIndex((el) => el.id === id);
+    newlist[index].editing = true;
+    // console.log(newlist[index].completed);
+    setState(newlist);
+  }
+
+  function editItem(formData) {
+    const newItem = formData.get("item");
+    const id = formData.get("itemindex");
+    const newlist = [...liststate];
+    const index = newlist.findIndex((el) => el.id === id);
+    newlist[index].name = newItem;
+    newlist[index].editing = false;
+    setState(newlist);
+  }
+
   const listitems = liststate.map((item) => (
     <>
       <li key={item.id}>{item.name}</li>
+      { item.editing &&
+        <form action={ (e) => { editItem(e)}}>
+          <input name="item" defaultValue={item.name} ></input>
+          {/* create a hidden readonly text field so itemindex gets passed in the formData */}
+          <input name="itemindex" value={item.id} style={{display: "none"}} readOnly={true}></input>
+          <button type="submit">Edit</button>
+        </form>
+      }
+      {
+        !item.editing &&
+        <button itemindex={item.id} onClick={(e) => enterEditMode(e.target.attributes.itemindex.value)}>Edit</button>
+      }
       <button itemindex={item.id} onClick={(e) => deleteItem(e.target.attributes.itemindex.value)}>Delete</button>
       <input type="checkbox" itemindex={item.id} checked={item.completed} onChange={(e) => updateCompletion(e.target.attributes.itemindex.value)} />
     </>
